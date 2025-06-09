9d77a5e9ef7d92ecc2788cfbd834215eb5b7dde4
Luciano <luciano@fior.in>
2025-06-09 06:45:57 -0300

    feat: abominations update

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -9,38 +9,13 @@ import {
   NavbarItem,
   NavbarMenuItem,
 } from "@heroui/navbar";
-import { Autocomplete, AutocompleteItem } from "@heroui/autocomplete";
 import { link as linkStyles } from "@heroui/theme";
 import NextLink from "next/link";
 import clsx from "clsx";
-import { useRouter } from "next/navigation";
 
 import { Logo } from "@/components/icons";
-import { useAbominations } from "@/hooks/useAbominations";
 
 export const Navbar = () => {
-  const { abominations } = useAbominations();
-  const router = useRouter();
-
-  const searchInput = (
-    <Autocomplete
-      aria-label="Search abominations"
-      className="w-full max-w-xs"
-      placeholder="Search..."
-      onSelectionChange={(key) => {
-        if (key) {
-          router.push(`/abominations/${key}`);
-        }
-      }}
-    >
-      {abominations.map((abomination) => (
-        <AutocompleteItem key={abomination.slug}>
-          {abomination.name}
-        </AutocompleteItem>
-      ))}
-    </Autocomplete>
-  );
-
   return (
     <HeroUINavbar maxWidth="xl" position="sticky">
       <NavbarContent className="basis-1/5 sm:basis-full" justify="start">
@@ -65,19 +40,11 @@ export const Navbar = () => {
         </ul>
       </NavbarContent>
 
-      <NavbarContent
-        className="hidden sm:flex basis-1/5 sm:basis-full"
-        justify="end"
-      >
-        <NavbarItem className="hidden lg:flex">{searchInput}</NavbarItem>
-      </NavbarContent>
-
       <NavbarContent className="sm:hidden basis-1 pl-4" justify="end">
         <NavbarMenuToggle />
       </NavbarContent>
 
       <NavbarMenu>
-        <div className="px-4">{searchInput}</div>
         <div className="mx-4 mt-4 flex flex-col gap-2">
           <NavbarMenuItem>
             <NextLink href="/abominations">Abominations</NextLink>
